560322183af8a8b3516feea1db38b1ac275da4a0
Alexander <sttp2002@gmail.com>
2023-06-20 00:02:41 +0300

    method is ready

--- Program.cs
+++ Program.cs
@@ -5,3 +5,13 @@
 //Console.WriteLine(length);
 string [] array = {"Hello", "2", "world", ":-)"};
 Console.WriteLine(String.Join(", ", array)); 
+void PrintArray (string[] arr){
+    int length = 0;
+    for (int i = 0; i < arr.Length; i++){
+        length = arr[i].Length;
+        if (length <=3) Console.Write($"{arr[i]} ,"); 
+
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
