952717a9667281b851a915ddd24b06dd4c452673
Alexander <sttp2002@gmail.com>
2023-06-19 23:37:30 +0300

    array created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+//string str = "abcde";
+//int length = str.Length;
+//Console.WriteLine(length);
+string [] array = {"Hello", "2", "world", ":-)"};
+Console.WriteLine(String.Join(", ", array)); 
