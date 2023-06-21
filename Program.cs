51c009aeced70d98ce64e78255b276f849a6451c
Alexander <sttp2002@gmail.com>
2023-06-22 00:48:54 +0300

    program is ready

--- Program.cs
+++ Program.cs
@@ -1,17 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-//string str = "abcde";
-//int length = str.Length;
-//Console.WriteLine(length);
-string [] array = {"Hello", "2", "world", ":-)"};
+string [] array = {"Hello", "2", "world", ":-)", "wxyz", "T9", "77", "qwerty", "yes"};
 Console.WriteLine(String.Join(", ", array)); 
 void PrintArray (string[] arr){
     int length = 0;
+    int j = 0;
+    string [] result = new string [arr.Length];
     for (int i = 0; i < arr.Length; i++){
         length = arr[i].Length;
-        if (length <=3) Console.Write($"{arr[i]} ,"); 
-
+        result[i] = String.Empty;
+        if (length <=3) {
+            result[j] = arr[i];
+            j++; 
+        }
     }
+    int lastIndex = Array.IndexOf(result, String.Empty);
+    for (int k = 0; k<lastIndex-1; k++){
+        Console.Write($"{result[k]}, ");
+        } 
+    Console.Write(result[lastIndex-1]);
     Console.WriteLine();
 }
 PrintArray(array);
